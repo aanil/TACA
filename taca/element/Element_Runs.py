db2b99e3bf39ea4c4a14da1367228e6b090b1b05
chuan-wang <chuan.wang@scilifelab.se>
2024-10-24 15:44:03 +0200

    Fix issue with redundant PhiX record

--- taca/element/Element_Runs.py
+++ taca/element/Element_Runs.py
@@ -953,19 +953,39 @@ class Run:
                 )
         # Remove redundant rows for PhiX
         aggregated_assigned_indexes_filtered = []
-        unique_phiX_combination = set()
+        phix_filtered = []
         for sample in aggregated_assigned_indexes:
             # Add project name
             sample["Project"] = [
                 d for d in demux_runmanifest if d["SampleName"] == sample["SampleName"]
             ][0]["Project"]
+            # Get the PhiX with the longest index combination.
             if sample["SampleName"] == "PhiX":
-                combination = (sample["I1"], sample["I2"], sample["Lane"])
-                if combination not in unique_phiX_combination:
-                    aggregated_assigned_indexes_filtered.append(sample)
-                    unique_phiX_combination.add(combination)
+                lane = sample["Lane"]
+                idx1 = sample["I1"]
+                idx2 = sample["I2"]
+                num_polonies_assigned = sample["NumPoloniesAssigned"]
+                if not phix_filtered:
+                    phix_filtered.append(sample)
+                else:
+                    found_flag = False
+                    for phix_record in phix_filtered:
+                        if lane == phix_record["Lane"]:
+                            idx1_shorter_len = min(len(idx1), len(phix_record["I1"]))
+                            idx2_shorter_len = min(len(idx2), len(phix_record["I2"]))
+                            if idx1[:idx1_shorter_len] == phix_record["I1"][:idx1_shorter_len] and idx2[:idx2_shorter_len] == phix_record["I2"][:idx2_shorter_len]:
+                                found_flag = True
+                                # When the new record has a longer index combination length, take the new record and remove the old one
+                                # When the index combination length happen to be the same, keep the one with the higher polonies assigned
+                                if len(idx1)+len(idx2) > len(phix_record["I1"])+len(phix_record["I2"]) or (len(idx1)+len(idx2) == len(phix_record["I1"])+len(phix_record["I2"]) and num_polonies_assigned >= phix_record["NumPoloniesAssigned"]):
+                                    phix_filtered.remove(phix_record)
+                                    phix_filtered.append(sample)
+                    if not found_flag:
+                        phix_filtered.append(sample)
             else:
                 aggregated_assigned_indexes_filtered.append(sample)
+        # Combine the list of samples and PhiX
+        aggregated_assigned_indexes_filtered += phix_filtered
         # Sort the list by Lane, SampleName and sub_demux_count
         aggregated_assigned_indexes_filtered_sorted = sorted(
             aggregated_assigned_indexes_filtered,
