b3d84467436005fb08f8f3354770442e6d3275fc
chuan-wang <chuan.wang@scilifelab.se>
2024-11-04 14:05:42 +0100

    Refactor based on comment from AA

--- taca/illumina/Runs.py
+++ taca/illumina/Runs.py
@@ -704,18 +704,11 @@ class Run:
         os.symlink(source, dest)
 
         # Replace the file laneBarcode.html
-        source_html_report_laneBarcode = os.path.join(
+        html_report_laneBarcode = os.path.join(
             source, "html", self.flowcell_id, "all", "all", "all", "laneBarcode.html"
         )
-        dest_html_report_laneBarcode = os.path.join(
-            dest, "html", self.flowcell_id, "all", "all", "all", "laneBarcode.html"
-        )
-        if os.path.exists(source_html_report_laneBarcode) and os.path.exists(
-            dest_html_report_laneBarcode
-        ):
-            html_report_laneBarcode_parser = LaneBarcodeParser(
-                source_html_report_laneBarcode
-            )
+        if os.path.exists(html_report_laneBarcode):
+            html_report_laneBarcode_parser = LaneBarcodeParser(html_report_laneBarcode)
             # Remove the trailing "_SX" postfix from samples names for BCL Convert when it handles SmartSeq3 libraries
             for entry in html_report_laneBarcode_parser.sample_data:
                 if "_S" in entry["Sample"]:
@@ -724,9 +717,7 @@ class Run:
                 html_report_laneBarcode_parser.sample_data,
                 key=lambda k: (k["Lane"].lower(), k["Sample"]),
             )
-            _generate_lane_html(
-                dest_html_report_laneBarcode, html_report_laneBarcode_parser
-            )
+            _generate_lane_html(html_report_laneBarcode, html_report_laneBarcode_parser)
 
     def _fix_html_reports_for_complex_lanes(
         self,
