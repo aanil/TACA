afad6ec1055e5c7ae4a6e45bae307dd8580a73b3
kedhammar <alfred.kedhammar@scilifelab.se>
2024-11-18 11:41:02 +0100

    add preproc nosync to test fixture

--- tests/nanopore/test_instrument_transfer.py
+++ tests/nanopore/test_instrument_transfer.py
@@ -30,6 +30,7 @@ def setup_test_fixture():
     for dir in [
         args.source_dir,
         args.dest_dir,
+        args.dest_dir + "/nosync",
         args.dest_dir_qc,
         args.archive_dir,
         args.minknow_logs_dir,
